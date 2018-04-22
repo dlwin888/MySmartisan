d2c75a4983353cef6ebce5fcebbdc0171b8ebd58
dlwin888 <duanle888@gmail.com>
2018-04-22 16:52:20 +0800

    home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import '@/assets/css/reset.css'
+import '@/assets/css/header.css'
 
 Vue.use(Router)
+import Shop from '@/views/shop'
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Shop',
+      component: Shop
     }
   ]
 })
